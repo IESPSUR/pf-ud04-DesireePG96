68112413aa772e3e76fa0d9078d2f48aa349b097
desireepg96 <desiree.perez.gomez.al@iespoligonosur.org>
2022-11-08 10:15:15 +0100

    intento listado

--- tienda/views.py
+++ tienda/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from tienda.models import Producto
 
 # Create your views here.
 def welcome(request):
@@ -6,5 +7,11 @@ def welcome(request):
 
 def listado(request):
     return render(request, 'tienda/listado.html', {})
+
+
+
+def book_list(request):
+    productos = Producto.objects.order_by('nombre')
+    return render('listado.html', {'productos': productos})
 
 
